48ce6f86f6ce364e62583e8c15c61493b8506932
Anjali <138780184+anjalikray@users.noreply.github.com>
2023-09-26 21:06:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,60 @@
+//change navbar style on scroll
+
+window.addEventListener('scroll' , () => {
+    document.querySelector('nav').classList.toggle('window-scroll' , window.scrollY > 0) 
+})
+
+//show/hide faq answers
+const faqs = document.querySelectorAll('.faq');
+
+faqs.forEach(faq => {
+    faq.addEventListener('click' , () =>{
+        faq.classList.toggle('open');
+
+        //change icon
+        const icon = faq.querySelector('.faq_icon i');
+        if(icon.className === "uil uil-plus"){
+            icon.className = "uil uil-minus";
+        }
+        else{
+            icon.className = "uil uil-plus";
+        }
+    });
+})
+
+//slider
+
+var swiper = new Swiper(".mySwiper", {
+slidesPerView: 1,
+spaceBetween: 30,
+pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    },
+
+    //when window width is >= 600px
+    breakpoints: {
+        600: {
+            slidesPerView: 2
+        }
+    }
+});
+
+// show/hide nav menu
+const menu = document.querySelector(".nav_menu");
+const menuBtn = document.querySelector("#open-menu-btn");
+const closeBtn = document.querySelector("#close-menu-btn");
+
+menuBtn.addEventListener('click' , () => {
+    menu.style.display = "flex";
+    closeBtn.style.display = "inline-block";
+    menuBtn.style.display = "none";
+})
+
+closeBtn.addEventListener('click' , () => {
+    menu.style.display = "none";
+    closeBtn.style.display = "none";
+    menuBtn.style.display = "inline-block";
+})
+
+
